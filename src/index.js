6ea432fb344c104111e90a06c4349fb0ad3c031a
Heilong <llybolayo@gmail.com>
2024-04-04 09:40:54 +0200

    uploaded some files

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <App />
-    <App /> 
-    <App /> 
-
+    
   </React.StrictMode>
 );
-//dont touch here -- 
