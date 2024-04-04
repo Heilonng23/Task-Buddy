e52ed04711789232766f885b8c9a4ccb91243d69
Heilong <llybolayo@gmail.com>
2024-04-04 10:40:18 +0200

    title changed

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ function App() {
   function handleSub(){
     setCount(count - 1)
   }
+  function handleReset(){
+    setCount(0)
+  }
   return (
     <div className='App'>
       <div className='box'>
         <p>{count}</p>
         <button onClick={handleAdd} className='add'>ADD</button>
         <button onClick={handleSub} className='sub'>SUB</button>
+        <button onClick={handleReset} className='reset'>RESET</button>
       </div>
     </div>
   );
